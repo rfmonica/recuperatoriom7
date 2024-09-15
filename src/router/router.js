c3d102d284e9141292bd0ab1c1eb55bbba48205f
rfmonica <rfmonica11@gmail.com>
2024-09-15 03:47:43 -0400

    Proyecto Deployado

--- src/router/router.js
+++ src/router/router.js
@@ -9,7 +9,7 @@ import App from "../App";
 import Traductor from "../screens/traductor/traductor.jsx"
 
 
-const basename = process.env.NODE_ENV === 'production' ? '/desarrollo-frontend-react' : '/';
+const basename = process.env.NODE_ENV === 'production' ? '/recuperatorio7' : '/';
 
 const routes = createBrowserRouter([
     {
